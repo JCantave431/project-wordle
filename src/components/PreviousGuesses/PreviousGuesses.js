ac98a54071bdea1d12179b51177b5c3d212081e4
JCantave431 <jenterra10@gmail.com>
2025-06-13 15:50:37 -0400

    added Guess component that displays each character of a guess in a box. Updated PreviousGuesses to always show 6 Guesses, whether empty or not.

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Guess from "../Guess";
 
-function PreviousGuesses({guesses}) {
-
+function PreviousGuesses({ guesses }) {
   return (
-    <div className="guess-results">
+    /*<div className="guess-results">
       {guesses.map((guess, index) => (
         <p key={index} className="guess">{guess}</p>
+      ))}
+    </div>*/
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num, index) => (
+        <Guess key={index} guess={guesses[index]}></Guess>
       ))}
     </div>
   );
